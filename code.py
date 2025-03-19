19fd85bd053f01b8664d54e2ec75f30b69705e8e
shuvankr7 <92281019+shuvankr7@users.noreply.github.com>
2025-03-19 12:35:29 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,5 @@
 import os
 import streamlit as st
-st.set_page_config(
-    page_title="Q&A Bot"
-)
 from langchain_groq import ChatGroq
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
